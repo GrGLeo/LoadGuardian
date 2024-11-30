8813b56525d8e72b4983816bf6bbe13a82b589e9
GrGLeo <graziani.gothe.leo@gmail.com>
2024-11-30 20:59:49 +0100

    started cleaning

--- main.go
+++ main.go
@@ -9,67 +9,6 @@ import (
 const URL string = ""
 var PORTS [2]string 
 
-type BackendService struct {
-  Endpoint string
-  Connection int
-}
-
-type LoadBalancer struct {
-  Services []BackendService
-  index uint8
-}
-
-func (lb *LoadBalancer) getNextBackend() string {
-  mod := uint8(len(lb.Services))
-  lb.index = (lb.index + 1) % mod
-  return lb.Services[lb.index].Endpoint
-}
-
-func (lb *LoadBalancer) handleRequest(w http.ResponseWriter, r *http.Request) {
-  BackendURL := lb.getNextBackend()
-  targetURL := BackendURL + r.URL.Path
-  fmt.Printf("\n1: %q\n", targetURL)
-
-  var body io.Reader
-      if r.Body != nil {
-          body = r.Body
-      } else {
-          body = http.NoBody
-      }
-
-  req, err := http.NewRequest(r.Method, targetURL, body)
-  fmt.Printf("\n2: %q\n", targetURL)
-  if err != nil {
-    http.Error(w, "Failed to create request", 500)
-    return
-  }
-  for key, values := range r.Header {
-    for _, value := range values {
-      req.Header.Add(key, value)
-    }
-  }
-  fmt.Printf("\n3: %q\n", targetURL)
-
-  client := &http.Client{}
-  resp, err := client.Do(req)
-  fmt.Printf("\n4: %q\n", targetURL)
-  if err != nil {
-    fmt.Printf("\n5: %q\n", targetURL)
-    http.Error(w, "Failed to forward request", 500)
-    return
-  }
-  defer resp.Body.Close()
-
-  for key, values := range resp.Header{
-    for _, value := range values {
-      w.Header().Add(key, value)
-    }
-  }
-  fmt.Printf("\n%q\n", targetURL)
-  
-  w.WriteHeader(resp.StatusCode)
-  io.Copy(w, resp.Body)
-}
 
 func main () {
   services := CreateBackendServices()
--- services.go
+++ services.go
@@ -11,29 +11,31 @@ import (
 func CreateBackendServices() []BackendService {
   var Services []BackendService
 
+  // Create Docker client
   cli, err := client.NewClientWithOpts(client.FromEnv, client.WithAPIVersionNegotiation())
   if err != nil {
       panic(err)
   }
   defer cli.Close()
 
+  // List all container
   containers, err := cli.ContainerList(context.Background(), container.ListOptions{All: true})
   if err != nil {
       panic(err)
   }
 
- for _, container := range containers {
+  // Loop through the containers & only continue on labelled one
+  for _, container := range containers {
     if container.Labels["LoadBalanced"] == "true" {
+
       containerInfo, err := cli.ContainerInspect(context.Background(), container.ID)
       if err != nil {
         fmt.Printf("Error inspecting container: %v", err)
       }
-      fmt.Printf("Container port: %v\n", container.Names[0])
-
+      // TODO: there should be a better way to do this.
       if len(containerInfo.NetworkSettings.Ports) > 0 {
-        for port, _ := range containerInfo.NetworkSettings.Ports {
-          fmt.Printf("Container port: %v\n", port)
-          Services = append(Services, BackendService{Endpoint: "http:/"+container.Names[0]+":"+port.Port(), Connection: 0})
+        for port := range containerInfo.NetworkSettings.Ports {
+          Services = append(Services, BackendService{ID: container.ID, Endpoint: "http:/"+container.Names[0]+":"+port.Port(), Connection: 0})
         }
       } else {
         fmt.Println("No ports exposed or mapped.")
